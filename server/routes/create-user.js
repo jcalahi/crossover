e3d60444c5691bbe9b3ad6474a45519661988cc6
juliuscalahi <juliuscalahi@users.noreply.github.com>
2016-04-07 02:37:38 +0800

    + SQL queries

--- server/routes/create-user.js
+++ server/routes/create-user.js
@@ -6,13 +6,24 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 var insertQuery = 'INSERT INTO users(name, coins) VALUES (?, ?)';
+var searchQuery = 'SELECT name FROM users WHERE name=?';
+
+router.use(function(req, res, next) {
+    connection.query(searchQuery, req.body.username, function(err, result) {
+        if (result.length > 0) {
+            console.log('exists');
+        } else {
+            console.log('not found');
+        }
+    });
+    next();
+});
 
 router.post('/createuser', function(req, res) {
     
-    connection.query(insertQuery, [req.body.username, 1000], function(err, result) {
+    /*connection.query(insertQuery, [req.body.username, 1000], function(err, result) {
         if (err) { throw err; }
-        console.log(result);
-    });
+    });*/
 	req.session.username = req.body.username;
     connection.end();
 	res.end();
